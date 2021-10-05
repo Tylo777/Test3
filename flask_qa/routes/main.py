55ce69a8adbb2d1308bc7b7e44f16bcb7d6337cd
Tylo Scott <scotylo@gmail.com>
2021-10-05 18:36:26 +0200

    Removed Questions

--- flask_qa/routes/main.py
+++ flask_qa/routes/main.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
 
 from flask_qa.extensions import db
-from flask_qa.models import Question, User
-#from flask_qa.models import User
+#from flask_qa.models import Question, User
+from flask_qa.models import User
 
 main = Blueprint('main', __name__)
 
