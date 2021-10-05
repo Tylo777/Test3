72100cca551ed82b9643bed80767a40b4680d26c
Tylo Scott <scotylo@gmail.com>
2021-10-05 18:28:19 +0200

    Removed Questions

--- flask_qa/models.py
+++ flask_qa/models.py
@@ -32,9 +32,9 @@ class User(UserMixin, db.Model):
     def unhashed_password(self, unhashed_password):
         self.password = generate_password_hash(unhashed_password)
 
-class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    question = db.Column(db.Text)
-    answer = db.Column(db.Text)
-    asked_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    appd_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+# class Question(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     question = db.Column(db.Text)
+#     answer = db.Column(db.Text)
+#     asked_by_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+#     appd_id = db.Column(db.Integer, db.ForeignKey('user.id'))
